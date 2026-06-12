8c14388777d352d65b807308b992757ab5a5158d
krsna-shukla <shuklakrish639@gmail.com>
2026-06-12 12:25:12 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -192,14 +192,14 @@ function Home({ search }) {
             <div
               key={product._id}
               onClick={() => navigate(`/product/${product._id}`)}
-              className="bg-white rounded-3xl shadow-lg p-5 cursor-pointer hover:scale-105 transition duration-300"
+              className="bg-white rounded-3xl shadow-lg p-5 cursor-pointer hover:scale-105 transition duration-300 flex flex-col h-full"
             >
               <img
   src={`https://ecommerce-mern-project-dimt.onrender.com${product.image}`}
   alt={product.name}
   className="w-full h-64 object-cover rounded-xl"
 />
-
+              
               <h2 className="text-xl font-bold mt-4 text-gray-800">
                 {product.name}
               </h2>
