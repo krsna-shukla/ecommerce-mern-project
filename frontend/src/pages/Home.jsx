ebd76cdb76ee38e0b7cb4d74771fc68647f0706d
krsna-shukla <shuklakrish639@gmail.com>
2026-06-12 12:29:48 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -186,7 +186,7 @@ function Home({ search }) {
       </div>
 
       {/* Products Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-10 pb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-stretch gap-8 px-10 pb-10">
         {filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
             <div
