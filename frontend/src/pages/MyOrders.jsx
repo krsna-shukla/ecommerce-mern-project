5edf0d402e0270a4681de46a9836716bddedac13
krsna-shukla <shuklakrish639@gmail.com>
2026-06-13 15:33:59 +0530

    Update MyOrders.jsx

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -25,7 +25,7 @@ function MyOrders() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-10 pt-32 md:pt-24">
+    <div className="min-h-screen bg-gray-100 p-10 pt-44 md:pt-28">
       <h1 className="text-4xl font-bold text-center mb-8">
         My Orders
       </h1>
