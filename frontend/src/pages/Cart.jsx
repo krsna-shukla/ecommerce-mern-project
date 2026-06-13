be2552ed9d0a93b753aba7a79cdb344ad95d729b
krsna-shukla <shuklakrish639@gmail.com>
2026-06-13 14:33:02 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -90,7 +90,7 @@ function Cart() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-10 pt-24">
+    <div className="min-h-screen bg-gray-100 p-10 pt-32 md:pt-24">
       <h1 className="text-4xl font-bold text-center mb-10">
         My Cart
       </h1>
